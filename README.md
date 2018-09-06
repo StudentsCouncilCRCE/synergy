ca45e732a01e3cd42e245d5832e9001a4bb0cac3
Sanfer Noronha <sansavio99@gmail.com>
2018-09-06 12:17:13 +0530

    First

---
+++ README.md
@@ -0,0 +1,2 @@
+# synergy.github.io
+SYNERGY 2017 - Fr. Conceicao Rodrigues College Of Engineering
---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp        = require('gulp');  
+var minifyCSS   = require('gulp-clean-css');  
+var concat      = require('gulp-concat');  
+var uglify      = require('gulp-uglify');  
+var imagemin    = require('gulp-imagemin');  
+var rename      = require('gulp-rename');  
+var sass        = require('gulp-sass');  
+
+//Change Folder paths
+
+gulp.task('styles',function(){  
+    var stylesSrc = [
+        './styles/development/scss/**/*.scss'
+    ];
+    return gulp.src(stylesSrc)
+        .pipe(sass())
+        .pipe(concat('dev.css'))
+        .pipe(gulp.dest('./styles/production/'))
+        .pipe(rename('prod.min.css'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('./styles/production/'));
+});
+
+gulp.task('scripts',function(){  
+    var scriptSrc =[
+        './js/development/vendor/**/*.js',
+        './js/development/foundation/**/*.js',
+        './js/development/app.js'
+
+    ];
+    gulp.src(scriptSrc)
+        .pipe(concat('dev.js'))
+        .pipe(gulp.dest('./js/production/'))
+        .pipe(rename('prod.min.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./js/production/'));
+});
+
+gulp.task('images',function(){  
+    return gulp.src('./images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./images'));
+});
+
+gulp.task('watch', function() {  
+    gulp.watch('./styles/development/**/*.scss', ['styles']);
+    gulp.watch('./js/development/**/*.js', ['scripts']);
+});
+
+gulp.task('default', ['styles','scripts','images']);
